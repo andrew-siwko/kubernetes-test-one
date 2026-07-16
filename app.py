49eea8aa5017691410e7eb772531578ca20db579
Andrew Siwko <asiwko@siwko.org>
2026-07-16 11:14:05 -0400

    add a  node list

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 import os
 
 PORT = 8000
+from kgetnodes import get_k8s_node_list
 
 class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -9,6 +10,9 @@ class MyHandler(SimpleHTTPRequestHandler):
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Hello! Your Python app is running inside Kubernetes, pulled from your local registry!\n")
+        for node in get_k8s_node_list():
+            self.wfile.write(f"{node}\n".encode())
+
 
 if __name__ == "__main__":
     print(f"Starting server on port {PORT}...")
